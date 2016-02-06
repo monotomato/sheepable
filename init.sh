a88ae41c9c9593c08804884e74e2d71f7cc1ffdb
Toni Pesola <pesola.toni@gmail.com>
2016-02-06 21:36:25 +0200

    Modified script

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo Init developent enviroment...
 git submodule update --init --recursive
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
 echo Job done.
